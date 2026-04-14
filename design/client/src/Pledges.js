baa8753274f5663e3cfdbd7c5d7fb39eb794a00e
sharmi37 <sharmilakumarknp@gmail.com>
2026-04-14 10:14:07 -0700

    Refactor Pledges component and update imports

--- design/client/src/Pledges.js
+++ design/client/src/Pledges.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "API";
+import API from "./Api.js";
 import { toast } from "react-toastify";
 
 const today = () => new Date().toISOString().split("T")[0];
