033ecf5cb23a9a615dcbe5dc5e852a2e9888e855
sharmi37 <sharmilakumarknp@gmail.com>
2026-04-14 10:12:57 -0700

    Refactor Home component and update imports

--- design/client/src/Home.js
+++ design/client/src/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "API";
+import API from "./Api.js";
 
 export default function Home() {
   const navigate = useNavigate();
