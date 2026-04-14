f3aba93ceceb0247ab5895a5a3cca1e73dd15eba
sharmi37 <sharmilakumarknp@gmail.com>
2026-04-14 10:13:22 -0700

    Refactor Items component and update imports

--- design/client/src/Items.js
+++ design/client/src/Items.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "API";
+import API from "./Api.js";
 import { toast } from "react-toastify";
 
 const empty = { name: "", category: "necklace", karat: "22K", weight_grams: "", description: "", market_price: "" };
