3d43b230a167322d7c03aec87beffb1c45c41880
sharmi37 <sharmilakumarknp@gmail.com>
2026-04-14 08:31:06 -0700

    Refactor Server.js to use DATABASE_URL

--- design/server/Server.js
+++ design/server/Server.js
@@ -8,13 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 pool.getConnection()
   .then(conn => {
